ef3f6daaddcc3466afe04664c7d7dd8bb8d2176f
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2024-07-25 13:13:46 +0530

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,344 @@
+import os, sys, re, requests, bs4, time, random, json, string
+from bs4 import BeautifulSoup
+from datetime import datetime
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests > /dev/null')
+try:
+    import bs4
+except ImportError:
+    print ('\n [×] Modul Bs4 Not installed!...\n')
+    os.system('pip install bs4')
+def convert(cok):
+    __for = 'datr=' + cok['datr'] + ';' + ('sb=' + cok['sb']) + ';' + ('fr=' + cok['fr']) + ';' + ('c_user=' + cok['c_user']) + ';' + ('xs=' + cok['xs'])
+    return __for
+def inbox(session):
+    time.sleep(1)
+    ses = requests.Session()
+    __ = str(time.time()).replace('.', '')[:13]
+    data = ses.get(f"https://10minutemail.net/address.api.php?sessionid={session}&_={str(__)}").json()
+    if len(data["mail_list"]) !=1:
+        address = data["mail_list"][0]["subject"]
+        session = address.replace('FB-', '').replace('is your Facebook confirmation code', '')
+        return session
+        print(inbox)
+def random_ua():
+    model = "iPhone"+str(random.randint(4,16))+','+str(random.randint(1,9))
+    abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+    build = str(random.randint(9,19))+random.choice(abc)+str(random.randint(50,199))
+    fbsv = str(random.randint(4,16))+'_'+str(random.randint(1,9))+'_'+str(random.randint(1,9))
+    ua1 = 'Mozilla/5.0 (iPhone, CPU iPhone '+fbsv+' like Mac OS '+str(random.randint(8,16))+') AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/'+build+') Safari/604.1'
+    ua2 = "Mozilla/5.0 (iPhone "+str(random.randrange(4,6))+" X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/"+str(random.randint(4,13))+".1.1 Mobile/"+model+" Safari/604.1"
+    dv_typ = random.choice(['SM-S911B','SM-S908B','SM-G998B','SM-G988B','SM-G973B','SM-N986B'])
+    ua3 = f"Mozilla/5.0 (Linux; Android {str(random.randint(4,13))}; "+dv_typ+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36"
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['RMX3686','RMX3393','RMX3081','RMX2170','RMX2061','RMX2020'])
+    bl_typ = random.choice(['QP1A','SKQ1','TP1A','RKQ1','SP1A','RP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua4 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['SM-S911B','SM-S908B','SM-G998B','SM-G988B','SM-G973B','SM-N986B'])
+    bl_typ = random.choice(['PPR1','LRX21T','TP1A','RKQ1','SP1A','RP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua5 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['vivo 1951','vivo 1918','V2011A','V2047','V2145','V2227A','V2160'])
+    bl_typ = random.choice(['RP1A','PKQ1','QP1A','TP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua6 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    ua = random.choice([ua1,ua2,ua3,ua4,ua5,ua6])
+    return(ua)
+def random_ua():
+    model = "iPhone"+str(random.randint(4,16))+','+str(random.randint(1,9))
+    abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+    build = str(random.randint(9,19))+random.choice(abc)+str(random.randint(50,199))
+    fbsv = str(random.randint(4,16))+'_'+str(random.randint(1,9))+'_'+str(random.randint(1,9))
+    ua1 = 'Mozilla/5.0 (iPhone, CPU iPhone '+fbsv+' like Mac OS '+str(random.randint(8,16))+') AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/'+build+') Safari/604.1'
+    ua2 = "Mozilla/5.0 (iPhone "+str(random.randrange(4,6))+" X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/"+str(random.randint(4,13))+".1.1 Mobile/"+model+" Safari/604.1"
+    dv_typ = random.choice(['SM-S911B','SM-S908B','SM-G998B','SM-G988B','SM-G973B','SM-N986B'])
+    ua3 = f"Mozilla/5.0 (Linux; Android {str(random.randint(4,13))}; "+dv_typ+") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36"
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['RMX3686','RMX3393','RMX3081','RMX2170','RMX2061','RMX2020'])
+    bl_typ = random.choice(['QP1A','SKQ1','TP1A','RKQ1','SP1A','RP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua4 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['SM-S911B','SM-S908B','SM-G998B','SM-G988B','SM-G973B','SM-N986B'])
+    bl_typ = random.choice(['PPR1','LRX21T','TP1A','RKQ1','SP1A','RP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua5 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    a = random.randrange(112,115)
+    b = random.randrange(1000,10000)
+    c = random.randrange(10,100)
+    os_ver = random.randrange(10,13)
+    dv_typ = random.choice(['vivo 1951','vivo 1918','V2011A','V2047','V2145','V2227A','V2160'])
+    bl_typ = random.choice(['RP1A','PKQ1','QP1A','TP1A'])
+    dv_ver = random.randrange(100000,250000)
+    sd_ver = random.randrange(1,10)
+    ch_ver = f'{a}.0.{b}.{c}'
+    ua6 = f'Mozilla/5.0 (Linux; Android {os_ver}; {dv_typ} Build/{bl_typ}.{dv_ver}.00{sd_ver}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{ch_ver} Mobile Safari/537.36'
+    ua = random.choice([ua1,ua2,ua3,ua4,ua5,ua6])
+    return(ua)
+logo4 = """
+\x1b[1;91m
+\x1b[1;92m
+\x1b[1;96m
+\x1b[1;92m         {|} {|}  {|} {|}{|}{|} {|}{|}{|}
+\x1b[1;97m         {|} {|}  {|}    {|}       {|}
+\x1b[1;93m         {|} {|}  {|}    {|}       {|}
+\x1b[1;96m         {|} {|}  {|}    {|}       {|}
+\x1b[1;94m     {|}{|}   {|}{|}     {|}       {|}
+\x1b[1;93m
+\x1b[1;92m         Jutt Badshah Brand~
+\x1b[1;91m-----------------------------------------------
+\x1b[1;97m> Author : Jutt Badshah
+\x1b[1;97m> Github : https://github.com/SHOOTER-MAKER
+\x1b[1;97m> Facebok: Jutt Badshah
+\x1b[1;97m> Version: auto create facebook
+\x1b[0;97m-----------------------------------------------"""
+
+
+boy = ['Ali Khan', 'Rustam Khan', 'Faisal Khan', 'Afzal Khan', 'Haider Khan', 'Suleman Khan', 'Nadeem Khan', 'Nazeer Malik', 'Nazeer Jutt', 'Nazeer Rehmani', 'Safdar Malik', 'Intzar Khan', 'Saleem Malik', 'Abdullah Malik', 'Naseer Jutt', 'Muzammil Malik', 'Fiaz Ahmad', 'Asghar Ali', 'Shabeer Ahmad', 'Irfan Ali', 'Ahmad Gujjar']
+girl = ['Sajida Malik', 'Ayesha Khan', 'Nabeela Malik', 'Kinza Fatima', 'Arooj Khan', 'Muskan Khan', 'Ayesha Malik', 'Safina Malik', 'Nida Ali', 'Rimsha Ali']
+ok = []
+cp = []
+def menu():
+    os.system('clear')
+    print (logo4)
+    print ('            Auto create tool')
+    print (47*'-')
+    print ('[1]auto creat')
+    print ('[2]join Facebok group')
+    print ('[3] join whatsapp group')
+    print (47*'-')
+    sel = input('Select: ')
+    if sel in['1', '01']:
+        create().start()
+    elif sel in ['2', '02']:
+        os.system('xdg-open https://www.facebook.com/groups/262660289344669/?ref=share_group_link')
+        menu()
+    elif sel in ['3', '03']:
+        os.system('xdg-open https://chat.whatsapp.com/C4EokyLxEaZGBlyJ99M3pA')
+        menu()
+    else:
+        print ('select valid option')
+        time.sleep(3)
+        menu()
+class create:
+
+    def __init__(self):
+        self.loop = 0
+        self.gender = []
+
+    def start(self):
+        os.system('clear')
+        print (logo4)
+        print ('[1] Boys name ids')
+        print ('[2] girls name ids')
+        print (47*'-')
+        gen = input('select: ')
+        print (47*'-')
+        if gen in ['1', '01']:
+            self.gender.append('boy')
+        elif gen in ['2', '02']:
+            self.gender.append('girl')
+        else:
+            self.gender.append('boy')
+        print ('Example 1000, 2000, 5000, 10000')
+        lim = int(input('limit: '))
+        os.system('clear')
+        print (logo4)
+        #agent = random.choice(ugen)
+        ua=random_ua()
+        headers = {
+            'authority': 'm.facebook.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"11.0.0"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': random_ua(),
+            'viewport-width': '980',
+        }
+        headers1 = {
+            'authority': 'm.facebook.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+            'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"11.0.0"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'upgrade-insecure-requests': '1',
+            'user-agent': random_ua(),
+        }
+        OO = '\033[0;97m'
+        for x in range(lim):
+            self.loop += 1
+            sys.stdout.write(f'\r {OO}[Creat-fb] {OO}{self.loop}/{str(lim)} OK:{len(ok)} - CP:{len(cp)}{OO} '),
+            sys.stdout.flush()
+            if 'boy' in self.gender:
+                name = random.choice(boy).split(' ')
+                sex = '2'
+            elif 'girl' in self.gender:
+                name = random.choice(girl).split(' ')
+                sex = '1'
+            try:
+                ses = requests.Session()
+                buildses = user = "".join(random.SystemRandom().choice("qwertyuiopasdfghjklzxcvbnm0987654321") for i in range(26))
+                create = ses.get(f"https://10minutemail.net/address.api.php?new=1&sessionid={buildses}&_={int(datetime.now().timestamp() * 1000)}").json()
+                mail = {"mail": create["permalink"]["mail"], "session": create["session_id"]}
+                email = mail['mail']
+                session = mail['session']
+            except KeyError:
+                pass
+            except requests.exceptions.ConnectionError:
+                time.sleep(1)
+                pass
+            except Exception as e:
+                pass
+            passw = name[0]+name[1]+str(random.randint(111,999))
+            try:
+                self.ses = requests.Session()
+                a = self.ses.get('https://m.facebook.com/reg?_fb_noscript', headers=headers)
+                loger = re.search('name="logger_id" value="(.*?)"', str(a.text)).group(1)
+                ref = BeautifulSoup(a.text, 'html.parser').find('form', {'action': True, "id":"mobile-reg-form", "method":"post"})
+                bl = ['lsd', 'jazoest', 'cpp', 'reg_instance', 'submission_request']
+                bz = ['reg_impression_id', 'ns']
+                self.data = {}
+                for v in ref('input'):
+                    if v.get('name') in bl:
+                        try:
+                            self.data.update({v.get('name'):v.get('value')})
+                        except:
+                            pass
+                self.data.update({'helper': ''})
+                for v in ref('input'):
+                    if v.get('name') in bz:
+                        try:
+                            self.data.update({v.get('name'):v.get('value')})
+                        except:
+                            pass
+                self.data.update({
+                    "zero_header_af_client": "",
+                    "app_id": "103",
+                    "logger_id": re.search('name="logger_id" value="(.*?)"', str(a.text)).group(1),
+                    "field_names[0]": "firstname",
+                    "firstname": name[0],
+                    "lastname": name[1],
+                    "field_names[1]": "birthday_wrapper",
+                    "birthday_day": str(random.randint(1,28)),
+                    "birthday_month": str(random.randint(1,12)),
+                    "birthday_year": str(random.randint(1992,2004)),
+                    "age_step_input": "",
+                    "did_use_age": "",
+                    "field_names[2]": "reg_email__",
+                    "reg_email__": str(random.randint(629,908))+str(random.randrange(99999,9999999)),
+                    "field_names[3]": "sex",
+                    "sex": sex,
+                    "preferred_pronoun": "",
+                    "custom_gender": "",
+                    "field_names[]": "reg_passwd__",
+                    "reg_passwd__": passw,
+                    "submit": "Sign Up",
+                    "name_suggest_elig": "false",
+                    "was_shown_name_suggestions": "false",
+                    "did_use_suggested_name": "false",
+                    "use_custom_gender": "",
+                    "guid": "",
+                    "pre_form_step": "",
+                })
+                gett = self.ses.post('https://m.facebook.com'+ref['action'], headers=headers1, data=self.data)
+                getts = self.ses.get('https://m.facebook.com/login/save-device/?login_source=account_creation&logger_id='+loger+'&app_id=103', headers=headers1)
+                data1 = {}
+                data2 = {}
+                data3 = {}
+                cok = self.ses.cookies.get_dict()
+                if 'checkpoint' in getts.url:
+                    cp.append(email+passw)
+                    print ('\r\033[1;33m[CP] '+cok['c_user']+' | '+passw+'\033[0;97m     ')
+                dbl = ['fb_dtsg', 'jazoest', 'flow', 'next', 'nux_source']
+                for x in BeautifulSoup(getts.text, 'html.parser').find_all('form', {'method': 'post'}):
+                    if '/login/device-based/update-nonce/' in str(x):
+                        for v in x('input'):
+                            if v.get('name') in dbl:
+                                try:
+                                    data1.update({v.get('name'):v.get('value')})
+                                except:
+                                    pass
+                        data1.update({'submit': 'OK'})
+                        po = self.ses.post('https://m.facebook.com'+x.get('action'), headers=headers1, data=data1)
+                        for x in BeautifulSoup(po.text, 'html.parser').find_all('form', {'method': 'post'}):
+                            if 'confirmation_event_location=cliff' in str(x):
+                                for v in x('input'):
+                                    if v.get('name') in dbl:
+                                        try:
+                                            data2.update({v.get('name'):v.get('value')})
+                                        except:
+                                            pass
+                                code = inbox(session)
+                                data2.update({'c': code, 'submit': 'Confirm'})
+                                rex = self.ses.post('https://m.facebook.com'+x.get('action'), headers=headers1, data=data2)
+                                if 'checkpoint' in rex.url:
+                                    cok = self.ses.cookies.get_dict()
+                                    cp.append(email+passw)
+                                    print ('\r\033[1;33m[CP] '+cok['c_user']+' | '+passw+'\033[0;97m     ')
+                                    wrt = '[XYZ-CP] %s|%' % (c_user,passw)
+                                    open('/sdcard/Auto-CP.txt', 'a').write('%s\n' % wrt)
+                                    open('/sdcard/AUTOCREATE_CP.txt','a').write('%s|%s\n' % c_user,passw)
+                                else:
+                                    coki = (";").join([ "%s=%s" % (key, value) for key, value in self.ses.cookies.get_dict().items() ])
+                                    cok = self.ses.cookies.get_dict()
+                                    print ('\r\033[1;32m[OK] '+cok['c_user']+' | '+passw+' | '+coki+'\033[0;97m     ')
+                                    open('/sdcard/AUTOCREATE_OK.txt','a').write(cok['c_user']+'|'+passw+ ' | ' +coki+'\n')
+                                    ok.append(email+passw)
+            except requests.exceptions.ConnectionError:
+                time.sleep(1)
+                pass
+            except Exception as e:
+                pass
+        print ('process has been completed')
+        print (47*'-')
+        print ('\033[1;32mTotal ids > Ok/' +str(len(ok)) + ' CP/' + str(len(cp)))
+        print (47*'-')
+        input('back')
+        menu()
+menu()
