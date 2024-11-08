18e2ba2d817b6a6e9127c90020875ebc922a6a77
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2024-11-08 13:05:46 +0530

    Add files via upload

---
+++ Number.py
@@ -0,0 +1,42 @@
+import random
+import os, sys, re, requests, bs4, time, random, json, string
+from bs4 import BeautifulSoup
+from datetime import datetime
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests > /dev/null')
+    
+b=random.choice(['7.0','8.1.0','9','10','11','12'])
+
+j=random.randrange(100000000)
+
+mail=f'rockeybhai1+XDBABY{j}@yandex.ru'
+
+pb=random.choice(['Vinay','Saroj','Ramesh','Alabagond','Bheem','Bheem','Pream','Singh','Rajesh','Yadav','Brijesh','Yadav','Sonu','Yadav','Aditya','Yadav','Sunil','Yadav','Sateyndar','Sateyndar','Raja','Abhishek','Yadav','Rikki','Raj','Rikki','Raj','Babita','Gour','Pintu','Kumar','Hero','Paswan','Md','Jahangir','Shyamdev','Kumar','Yadav','Deepak','Yadav','Akhilesh','Yadav','Kaju','Yadav','Chandan','Chaudhari','Mukeshkumragupta','Mukesh','Prakash','Kumar','Suryakant','Pandey','Viswash','Viswash','Yadav','Vinod','Vinod','Om','Prakash','Bind','Sanjay','Yadav','Wakif','Shekh','Pintu','Pintukumar','Akash','Yadav','CB','Rakesh','Chawan','Vikas','Yadav','Rajput','Prabhas','Ramesh','Gehlot','Kanhaiya','Kushwaha','Aanad','Kumar','Gulshan','Mishra','Subash','Maurya','Santosh','Kumar','Satyaveer','Yadav','Lav','Vishwakarma','Lokesha','VErla','Lokesha','VErla','Saynje','Nishad','Karthik','Sarkar','Ramghan','Kumar','Sonu','Kumar','Rajubhia','Raju','Sachi','Kumar','Anuj','Kumar','Jitendra','Kumar','Mayur','Patel','Anand','Gudhu','Kumar','Gudhu','Ganga','Ram','Taral','Vinod','Gorakh','Sharma','Manoj','Mahanta','Mahbob','Noor','Monu','Kumar','Diivakar','Yadav','Ramesh','Yadav','Ramvilas','Chaudhari','Ankush','Nishad','AN','Sahil','Rahmani','Ramesh','Shing','Sujit','Kumar','Sharma','Shweta','Bharti','Jay','Ram','Mou','Rou','Rakesh','Mallik','Sachin','King','Sahiba','Afrin','Tanish','Dubey','Akash','Yadav','Upendar','Chaudhary','Shaban','Ali','Mangrio'])
+
+pb2=random.choice(['Vinay','Saroj','Ramesh','Alabagond','Bheem','Bheem','Pream','Singh','Rajesh','Yadav','Brijesh','Yadav','Sonu','Yadav','Aditya','Yadav','Sunil','Yadav','Sateyndar','Sateyndar','Raja','Abhishek','Yadav','Rikki','Raj','Rikki','Raj','Babita','Gour','Pintu','Kumar','Hero','Paswan','Md','Jahangir'])
+
+boy=random.choice(['Ali Khan', 'Rustam Khan', 'Faisal Khan', 'Afzal Khan', 'Haider Khan', 'Suleman Khan', 'Nadeem Khan', 'Nazeer Malik', 'Nazeer Jutt', 'Nazeer Rehmani', 'Safdar Malik', 'Intzar Khan', 'Saleem Malik', 'Abdullah Malik', 'Naseer Jutt', 'Muzammil Malik', 'Fiaz Ahmad', 'Asghar Ali', 'Shabeer Ahmad', 'Irfan Ali', 'Ahmad Gujjar'])
+girl=random.choice(['Sajida Malik', 'Ayesha Khan', 'Nabeela Malik', 'Kinza Fatima', 'Arooj Khan', 'Muskan Khan', 'Ayesha Malik', 'Safina Malik', 'Nida Ali', 'Rimsha Ali'])
+
+Xd='629'
+Num=f' {Xd}{j}'
+
+nmp = ''.join(random.choice(string.digits) for _ in range(10))
+
+FUCK = str(random.randint(629,908))+str(random.randrange(99999,9999999))
+
+vds ='+91629'+str(random.randrange(9,49))+str(random.randint(9999,99999))
+
+#print(vds)
+print('XD-BOY')
+print('')
+print(f'FIRST NAME BOY:   '+boy)
+print('')
+print(f'LIRST NAME GIRL:  '+girl)
+print('')
+print(f'Number    :  '+vds)
+print('')
+print(f'TempMail  : '+mail);time.sleep(20)
+os.system('clear')
